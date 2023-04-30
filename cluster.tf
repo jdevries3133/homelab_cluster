2e3f722822d9fa6874c1f27437017051316bf5da
Jack DeVries <jdevries3133@gmail.com>
2023-04-29 22:45:41 -0400

    feat: certificate issuer

--- cluster.tf
+++ cluster.tf
@@ -33,17 +33,17 @@ resource "kubernetes_namespace" "nginx_ingress" {
 }
 
 resource "helm_release" "nginx_ingress" {
-  name = "nginx-ingress"
-  namespace = kubernetes_namespace.nginx_ingress.metadata.0.name
+  name       = "nginx-ingress"
+  namespace  = kubernetes_namespace.nginx_ingress.metadata.0.name
   repository = "https://helm.nginx.com/stable"
-  chart = "nginx-ingress"
-  version = "0.16.0"
+  chart      = "nginx-ingress"
+  version    = "0.16.0"
 
   set {
     // Using a DaemonSet ensures at least 1 runs per node, which means that any
     // worker node becomes a valid ingress recipient, which just makes life
     // simpler
-    name = "controller.kind"
+    name  = "controller.kind"
     value = "daemonset"
   }
   set {
@@ -51,15 +51,76 @@ resource "helm_release" "nginx_ingress" {
     // is going to integrate with cloud providers. For an on-prem homelab
     // setup, we want the ingress to listen on ports on the node they are
     // running on
-    name = "controller.service.type"
+    name  = "controller.service.type"
     value = "NodePort"
   }
   set {
-    name = "controller.service.nodePorts.http"
+    name  = "controller.service.nodePorts.http"
     value = 80
   }
   set {
-    name = "controller.service.nodePorts.http"
+    name  = "controller.service.nodePorts.http"
     value = 443
   }
 }
+
+resource "kubernetes_namespace" "certmanager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "helm_release" "certmanager" {
+  name       = "cert-manager"
+  namespace  = kubernetes_namespace.certmanager.metadata.0.name
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "1.11.1"
+
+  set {
+    name  = "installCRDs"
+    value = true
+  }
+}
+
+variable "letsencrypt_email" {
+  type = string
+}
+
+resource "kubernetes_manifest" "cluster_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "letsencrypt-prod"
+    }
+    spec = {
+      acme = {
+        email  = var.letsencrypt_email
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+        privateKeySecretRef = {
+          name = "issuer-account-key"
+        }
+        solvers = [
+          {
+            http01 = {
+              ingress = {
+                class = "public"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+}
+
+resource "kubernetes_storage_class" "default" {
+  metadata {
+    name = "default"
+  }
+  storage_provisioner = "kubernetes.io/no-provisioner"
+  volume_binding_mode = "WaitForFirstConsumer"
+}
+
+
