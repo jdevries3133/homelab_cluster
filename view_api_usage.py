d91f2827a3d982750427873a032303513ba7e0ea
Jack DeVries <jdevries3133@gmail.com>
2024-03-24 09:17:33 -0400

    feat: API usage viewer

---
+++ view_api_usage.py
@@ -0,0 +1,26 @@
+"""
+
+Usage:
+
+    kubectl get all --all-namespaces -o json | python3 main.py
+
+Easily take a peek at the APIs in use inside a cluster.
+"""
+
+import sys
+import json
+
+input = sys.stdin.read()
+data = json.loads(input)
+
+api_users = {}
+for item in data['items']:
+    key = f'{item["apiVersion"]}/{item["kind"]}'
+    api_users.setdefault(key, [])
+    api_users[key].append(item['metadata']['name'])
+
+for api, users in api_users.items():
+    print(f'=== {api} ===')
+    print(', '.join(users))
+    print('=' * (len(api) + 8))
+    print('\n')
